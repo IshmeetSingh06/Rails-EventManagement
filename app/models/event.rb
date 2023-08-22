aa394cb9f8666df37dff4cc5acd870a5daa03c51
Ishmeet Singh <singhishmeet16@gmail.com>
2023-08-22 11:21:48 +0530

    Updated the associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,14 @@
 class Event < ApplicationRecord
-  belongs_to :user, foreign_key: 'organizer_id'
+  scope :active, -> { where cancelled: false }
+
+  belongs_to :organizer, class_name: 'User', foreign_key: 'organizer_id'
   has_many :registrations
-  has_many :users, through: :registrations
+  has_many :attendees, through: :registrations, source: :user
 
   validates :name, presence: true
   validates :description, presence: true
   validates :location, presence: true
   validates :date_time, presence: true
-  validates :organizer, presence: true
+  validates :organizer_id, presence: true
   validates :capacity, presence: true, numericality: { greater_than: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
   enum role: { admin: 0, guest: 1 }
-  has_many :event
-  has_many :event, through: :registration
+
   has_many :registrations
+  has_many :organized_events, class_name: 'Event', foreign_key: 'organizer_id'
+  has_many :attended_events, through: :registrations, source: :event, -> { Event.active }
 
   validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
   validates :first_name, presence: true
   validates :last_name, presence: false, allow_nil: true
   validates :password, presence: true
