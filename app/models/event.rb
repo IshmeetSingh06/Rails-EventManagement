67957e10b8bc5284d2212b44f1a6e01cdb52e7a4
Ishmeet Singh <singhishmeet16@gmail.com>
2023-08-22 14:59:44 +0530

    Updated the validations and schema

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,14 +1,10 @@
 class Event < ApplicationRecord
-  scope :active, -> { where cancelled: false }
-
   belongs_to :organizer, class_name: 'User', foreign_key: 'organizer_id'
   has_many :registrations
   has_many :attendees, through: :registrations, source: :user
 
-  validates :name, presence: true, length: { in: 6..20 }
-  validates :description, presence: true, length: { maximum: 500 }
-  validates :location, presence: true, length: { in: 6..20 }
-  # validates :date_time, presence: true, date_time: true
-  validates :organizer_id, presence: true
+  validates_presence_of :name, :description, :location, :organizer_id, :time
   validates :capacity, presence: true, numericality: { greater_than: 0 }
+
+  scope :active, -> { where cancelled: false }
 end
