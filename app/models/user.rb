923b8ec1a8f485170d599a95cf9ea53b1544a82b
Ishmeet Singh <singhishmeet16@gmail.com>
2023-08-22 15:44:13 +0530

    Updated User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,7 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
-  validates_presence_of :first_name, :last_name, :password
-  validates :role, presence: true, inclusion: { in: role_types.keys }
+  validates_presence_of :first_name, :password, :role
 
-  enum role_types: { admin: 0, guest: 1 }
+  enum role: { admin: 0, guest: 1 }
 end
