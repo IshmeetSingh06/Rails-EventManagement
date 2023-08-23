3c4b7068fae88b34dd0131c4e23374fa60c3d4f2
Ishmeet Singh <singhishmeet16@gmail.com>
2023-08-23 16:21:08 +0530

    Added Active field in User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_22_083751) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_23_104724) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,6 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_083751) do
     t.datetime "updated_at", null: false
     t.string "email"
     t.string "authentication_token"
+    t.boolean "active", default: true
   end
 
   add_foreign_key "events", "users", column: "organizer_id"
